b33b754d601d2ea59fff312da69bef07524d27ce
Tristan-O-Keefe <tristan.o.keefe@ibm.com>
2020-02-27 16:50:47 +0000

    Changed error to not found
    Signed-off-by: Tristan-O-Keefe <tristan.o.keefe@ibm.com>

--- src/main/java/com/raiseGreen/rg_auth_svc/errorHandlers/DocNotFoundException.java
+++ src/main/java/com/raiseGreen/rg_auth_svc/errorHandlers/DocNotFoundException.java
@@ -3,7 +3,7 @@ package com.raiseGreen.rg_auth_svc.errorHandlers;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.FORBIDDEN)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class DocNotFoundException extends RuntimeException {
     public DocNotFoundException(String message) {
         super(message);
